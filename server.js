6dafd443d44850f160737061e939da78afa560a1
Mokych Andrey <mokych.andrey@apriorit.com>
2020-10-21 17:11:13 +0300

    Basic adaptation for lecture

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+
+app.use(bodyParser.json());
+let storage = [];
+
+function getMsg(index) {  
+  return index < storage.length ? {
+    id: index,
+    msg: storage[index]
+  } : null;
+}
+
+app.get('/api/values', function (req, res) {
+  res.send(JSON.stringify(storage.map((el, i) => getMsg(i))));
+});
+
+app.post('/api/values', function (req, res) {
+  storage.push(req.body.msg);  
+  res.send(JSON.stringify(getMsg(storage.length - 1)));
+});
+
+app.put('/api/values/:id', function (req, res) {
+  const msg = getMsg(req.params.id);
+  if (msg) {
+    storage[req.params.id] = req.body.msg || "";
+  }
+  res.send(JSON.stringify(getMsg(req.params.id)));
+});
+
+app.delete('/api/values/:id', function (req, res) {
+  if (getMsg(req.params.id)) {
+    storage.splice(req.params.id, 1);
+  }
+  res.send('');
 });
 
 app.listen(PORT);
